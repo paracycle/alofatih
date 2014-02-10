63c6dd6633e497557cc9e2a85b60c58d2db84d6f
Ufuk Kayserilioglu <ufuk@enkuba.com>
2014-02-10 20:15:45 +0200

    Keep CNAME file all the time and fix glyphicon path.

--- Gruntfile.js
+++ Gruntfile.js
@@ -105,7 +105,8 @@ module.exports = function (grunt) {
                     src: [
                         '.tmp',
                         '<%= yeoman.dist %>/*',
-                        '!<%= yeoman.dist %>/.git*'
+                        '!<%= yeoman.dist %>/.git*',
+                        '!<%= yeoman.dist %>/CNAME'
                     ]
                 }]
             },
